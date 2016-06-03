57e7c4976911cb4b9c55725b7f8665e82c37cc98
Christina Branson <clbranson@gmail.com>
2016-06-03 16:46:26 +0000

    Modified footer

--- footer.php
+++ footer.php
@@ -10,9 +10,19 @@
  */
 ?>
 
-		</div><!-- .site-content -->
+<div style="clear:both;"></div>
+		<footer>
+		<div class="row">
+			<div class="large-4 columns">
 
-		<footer id="colophon" class="site-footer" role="contentinfo">
+			</div>
+			<div class="large-4 columns">
+
+			</div>
+			<div class="large-4 columns">
+
+			</div>
+			<div class="large-4 columns">
 			<?php if ( has_nav_menu( 'primary' ) ) : ?>
 				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'foundation' ); ?>">
 					<?php
@@ -23,33 +33,9 @@
 					?>
 				</nav><!-- .main-navigation -->
 			<?php endif; ?>
+			</div>
 
-			<?php if ( has_nav_menu( 'social' ) ) : ?>
-				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'foundation' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'social',
-							'menu_class'     => 'social-links-menu',
-							'depth'          => 1,
-							'link_before'    => '<span class="screen-reader-text">',
-							'link_after'     => '</span>',
-						) );
-					?>
-				</nav><!-- .social-navigation -->
-			<?php endif; ?>
-
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the foundation footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'foundation_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'foundation' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'foundation' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+		</div>
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
 </div><!-- .site -->
