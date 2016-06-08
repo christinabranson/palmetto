973b5ab0c26b88e99c812128dc08a2d3480bcbc5
Christina Branson <clbranson@gmail.com>
2016-06-08 19:58:54 +0000

    Fixed styling and comments for page content template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,21 +9,7 @@
 ?>
 <section id="post">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<?php
-			$thumb_id = get_post_thumbnail_id();
-			$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-			$thumb_url = $thumb_url_array[0];
-		?>
-
-		<?php if ( $thumb_url && !strpos($thumb_url, 'wp-includes') ) { ?>
-		<header class="entry-header" style="background-image: url('<?php echo $thumb_url; ?>');">
-			<?php //the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header><!-- .entry-header -->
-		<?php } ?>
-
 		<div class="row">
-			<div class="large-8 columns">
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 				<?php foundation_meta_edit(); ?>
 				<?php foundation_post_thumbnail(); ?>
@@ -40,11 +26,6 @@
 						) );
 					?>
 				</div><!-- .entry-content -->
-
-			</div>
-			<div class="large-4 columns">
-				<?php get_sidebar( 'page' ); ?>
-			</div>
-		</div>
-	</article><!-- #post-## -->
-</section>
+		</div> <!-- row -->
+	</article> <!-- article -->
+</section> <!-- section -->
