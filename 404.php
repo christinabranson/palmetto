bcf86f01143c5f85ee4728181c754bea6eb4b6d6
Christina Branson <clbranson@gmail.com>
2016-06-19 23:17:19 +0000

    Updated code for 404 page

--- 404.php
+++ 404.php
@@ -3,32 +3,21 @@
  * The template for displaying 404 pages (not found)
  *
  * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
+ * @subpackage Palmetto
+ * @since Palmetto 0.1
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'foundation' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'foundation' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
+<div class="row">
+	<div class="large-8 columns">
+		<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'foundation' ); ?></h1>
+			<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'foundation' ); ?></p>
+			<?php get_search_form(); ?>
 	</div><!-- .content-area -->
+	<div class="large-4 columns">
+		<?php get_sidebar( 'page' ); ?>
+	</div>
+</div> <!-- row -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
