b9ad9d44abf891df121d4717e44d9905c9a7eef0
Christina Branson <clbranson@gmail.com>
2016-06-20 00:23:22 +0000

    Add featured property information to 404 page

--- 404.php
+++ 404.php
@@ -9,6 +9,32 @@
 
 get_header(); ?>
 
+<!-- Show featured image -->
+<?php
+	$featued_pre_text = get_theme_mod( 'featured_property_pre_text', '' );
+	$featured_name = get_theme_mod( 'featured_property_property_header', '' );
+	$featured_num_bedrooms = get_theme_mod( 'featured_property_number_bedrooms', '' );
+	$featured_num_bathrooms = get_theme_mod( 'featured_property_number_bathrooms', '' );
+	$featured_num_cars = get_theme_mod( 'featured_property_number_cars', '' );
+	$featured_url = get_theme_mod( 'featured_property_listing_url', '' );
+
+?>
+<?php if ( has_header_image() ) { ?>
+	<header class="front-page" style="background-image: url('<?php header_image(); ?>')";>
+		<div class="featured-property">
+			<div class="row">
+			<span class="featured"><?php if ($featued_pre_text) { echo $featued_pre_text; } ?>
+								   <?php if ($featured_name) { echo $featured_name; } ?></span><br/>
+			<span class="featured-sub">
+				<?php if ($featured_num_bedrooms) { ?><i class="fa fa-bed"></i> <?php echo $featured_num_bedrooms; ?> Bedrooms | <?php } ?>
+				<?php if ($featured_num_bathrooms) { ?><i class="fa fa-fire-extinguisher icon-rotate-90"></i> <?php echo $featured_num_bathrooms; ?> Bathrooms | <?php } ?>
+				<?php if ($featured_num_cars) { ?><i class="fa fa-car"></i> <?php echo $featured_num_cars; ?> Car | <?php } ?>
+				<?php if ($featured_url) { ?><a href="<?php echo $featured_url; ?>">View Listing</a></span> <?php } ?>
+			</div>
+		</div>
+	</header>
+<?php } ?>
+
 <div class="row">
 	<div class="large-8 columns">
 		<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'foundation' ); ?></h1>
